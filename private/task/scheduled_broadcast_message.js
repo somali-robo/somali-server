b985a8f6af24d5f83bebc5bab58b2725dd1b67a3
古川信行 <t_sakura@m1.ffn.ne.jp>
2017-04-25 14:56:00 +0900

    scheduled_broadcast_messageパス修正

--- private/task/scheduled_broadcast_message.js
+++ private/task/scheduled_broadcast_message.js
@@ -1,8 +1,8 @@
 /** 定期実行メッセージ
 */
 import {Meteor} from 'meteor/meteor';
-import {BroadcastMessage,BroadcastMessages} from '../imports/api/db/broadcast_message';
-import {ScheduledBroadcastMessage,ScheduledBroadcastMessages} from '../imports/api/db/scheduled_broadcast_message';
+import {BroadcastMessage,BroadcastMessages} from '../../imports/api/db/broadcast_message';
+import {ScheduledBroadcastMessage,ScheduledBroadcastMessages} from '../../imports/api/db/scheduled_broadcast_message';
 
 if (Meteor.isClient) {
   Meteor.startup(() => {
