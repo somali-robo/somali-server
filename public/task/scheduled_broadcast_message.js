d703a8b3dfe3cafe48d09316e9b861f0f622f571
古川信行 <t_sakura@m1.ffn.ne.jp>
2017-04-25 15:15:42 +0900

    scheduled_broadcast_message配置先変更

--- public/task/scheduled_broadcast_message.js
+++ public/task/scheduled_broadcast_message.js
@@ -4,11 +4,10 @@ import {Meteor} from 'meteor/meteor';
 import {BroadcastMessage,BroadcastMessages} from '../../imports/api/db/broadcast_message';
 import {ScheduledBroadcastMessage,ScheduledBroadcastMessages} from '../../imports/api/db/scheduled_broadcast_message';
 
+console.log("scheduled_broadcast_message.js");
 if (Meteor.isClient) {
   Meteor.startup(() => {
     Meteor.subscribe('scheduled_broadcast_messages');
     Meteor.subscribe('broadcast_messages');
   });
-
-  console.log("scheduled_broadcast_message.js");
 }
